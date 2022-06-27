c96ac96d355066f4503d481b6e19fd1f191d9141
Maria Hidalgo <maria.hidalgo@deliveryhero.com>
2022-06-27 03:29:15 +0200

    Add training with ViT model and EuroSAT dataset.

---
+++ ViT/dataset.py
@@ -0,0 +1,60 @@
+import os
+
+import torch
+from torch.utils.data import Dataset
+from torchvision.datasets import ImageFolder
+from torchvision.datasets.folder import default_loader
+from torchvision.datasets.utils import check_integrity, download_and_extract_archive
+
+ROOT_TMP = "/scratch/tmp/mhidalgo/data/"
+# ROOT_TMP = "data"
+URL = "https://madm.dfki.de/files/sentinel/EuroSAT.zip"
+MD5 = "c8fa014336c82ac7804f0398fcb19387"
+SUBDIR = "2750"
+
+
+def random_split(dataset, ratio=0.9, random_state=None):
+    if random_state is not None:
+        state = torch.random.get_rng_state()
+        torch.random.manual_seed(random_state)
+    n = int(len(dataset) * ratio)
+    split = torch.utils.data.random_split(dataset, [n, len(dataset) - n])
+    if random_state is not None:
+        torch.random.set_rng_state(state)
+    return split
+
+
+class EuroSAT(ImageFolder):
+    def __init__(self, root=ROOT_TMP, transform=None, target_transform=None):
+        self.download(root)
+        root = os.path.join(root, SUBDIR)
+        super().__init__(root, transform=transform, target_transform=target_transform)
+
+    @staticmethod
+    def download(root):
+        if not check_integrity(os.path.join(root, "EuroSAT.zip")):
+            download_and_extract_archive(URL, root, md5=MD5)
+
+
+# Apparently torchvision doesn't have any loader for this so I made one
+# Advantage compared to without loader: get "for free" transforms, DataLoader
+# (workers), etc
+class ImageFiles(Dataset):
+    """
+    Generic data loader where all paths must be given
+    """
+
+    def __init__(self, paths: [str], loader=default_loader, transform=None):
+        self.paths = paths
+        self.loader = loader
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, idx):
+        image = self.loader(self.paths[idx])
+        if self.transform is not None:
+            image = self.transform(image)
+        # WARNING -1 indicates no target, it's useful to keep the same interface as torchvision
+        return image, -1
